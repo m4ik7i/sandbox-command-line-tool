cc4d40d0874abaf887c9a106543fd6669b83567a
m4ik7i <m4ik7i@gmail.com>
2020-03-22 01:38:17 +0900

    refactor: sandbox-command-line-tool

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::{crate_name, crate_version, App};
+use command_macros::cmd;
+use std::ffi::OsStr;
+use std::io::{self, Write};
+use std::process::{Command, ExitStatus, Output};
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -7,29 +11,44 @@ fn main() -> Result<()> {
     let app = App::new(crate_name!()).version(crate_version!());
     let _ = app.get_matches();
 
-    exec("ls", vec![])?;
-    exec("rustc", vec!["--version"])?;
+    let (status, output) = exec("ls", &[] as &[&str])?;
+    if !status.success() {
+        write_stderr(&output)?;
+    };
+    write_stdout(&output)?;
+
+    let (status, output) = exec("rustc", &["--version"])?;
+    if !status.success() {
+        write_stderr(&output)?;
+    };
+    write_stdout(&output)?;
+
+    #[rustfmt::skip]
+    let output = cmd!(echo ("Hello, World!")).output()?;
+    if !output.status.success() {
+        write_stderr(&output)?;
+    };
+    write_stdout(&output)?;
 
     Ok(())
 }
 
-use std::ffi::OsStr;
-use std::io::{self, Write};
-use std::process::Command;
-
-fn exec<I, S>(command: S, args: I) -> io::Result<()>
+fn exec<C, I, S>(command: C, args: I) -> io::Result<(ExitStatus, Output)>
 where
+    C: AsRef<OsStr>,
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
 {
     let output = Command::new(command).args(args).output()?;
+    Ok((output.status, output))
+}
 
-    io::stdout().lock().write_all(&output.stdout)?;
+fn write_stdout(output: &Output) -> io::Result<()> {
+    io::stdout().write_all(&output.stdout)?;
+    Ok(())
+}
 
-    if !output.status.success() {
-        println!("status: {}", output.status);
-        io::stderr().lock().write_all(&output.stderr)?;
-    }
-
+fn write_stderr(output: &Output) -> io::Result<()> {
+    io::stderr().write_all(&output.stderr)?;
     Ok(())
 }
